938ce4cf4d06f12a6ed276cd59fdd7f7005677e9
RaelNehring <rsn.nehring@gmail.com>
2024-04-13 17:11:13 -0300

    .

--- app.py
+++ app.py
@@ -32,15 +32,26 @@ class CorsMiddleware(object):
 
     def __call__(self, environ, start_response):
         def custom_start_response(status, headers, exc_info=None):
-            headers.append(('Access-Control-Allow-Origin', '*'))
-            headers.append(('Access-Control-Allow-Methods', '*'))
-            headers.append(('Access-Control-Allow-Headers', '*'))
+            # Aplicando cabeçalhos CORS a todas as respostas
+            cors_headers = [
+                ('Access-Control-Allow-Origin', '*'),  # Permite requisições de qualquer origem
+                ('Access-Control-Allow-Methods', 'POST, GET, OPTIONS'),  # Especifica métodos permitidos
+                ('Access-Control-Allow-Headers', 'Content-Type, SOAPAction'),  # Especifica cabeçalhos permitidos
+            ]
+            headers.extend(cors_headers)
             return start_response(status, headers, exc_info)
 
+        if environ['REQUEST_METHOD'] == 'OPTIONS':
+            # Tratando requisições OPTIONS de forma adequada
+            custom_start_response('204 No Content', [])
+            return [b'']
+
+        # Inclui tratamento para a rota /status como antes
         if environ['REQUEST_METHOD'] == 'GET' and environ['PATH_INFO'] == '/status':
             start_response('200 OK', [('Content-Type', 'text/plain')])
             return [b"Service is up and running"]
 
+        # Chama a aplicação com os cabeçalhos CORS adicionados
         return self.app(environ, custom_start_response)
 
 wsgi_app = WsgiApplication(application)
